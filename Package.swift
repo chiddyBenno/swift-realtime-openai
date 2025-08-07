cf84db5d7999c02961b8f8caa9365cf179f9fe76
Benjamin Nadas <benjamin.nadas@stud-mail.uni-wuerzburg.de>
2025-08-07 14:04:43 +0200

    Remove WebRTC support from package

--- Package.swift
+++ Package.swift
@@ -15,10 +15,8 @@ let package = Package(
 	products: [
 		.library(name: "OpenAIRealtime", type: .static, targets: ["OpenAIRealtime"]),
 	],
-	dependencies: [
-		.package(url: "https://github.com/stasel/WebRTC.git", branch: "latest"),
-	],
+	dependencies: [],
 	targets: [
-		.target(name: "OpenAIRealtime", dependencies: ["WebRTC"], path: "./src"),
+		.target(name: "OpenAIRealtime", dependencies: [], path: "./src"),
 	]
 )
